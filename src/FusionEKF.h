35a0223977c6beb55c86097234d5e559e06edc75
Usedlobster <usedlobster@gmail.com>
2018-01-14 00:06:05 +0000

    Refactor

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -26,8 +26,8 @@ public:
   */
   void ProcessMeasurement(const MeasurementPackage &measurement_pack);
 
-  /**
-  * Kalman Filter update and prediction math lives in here.
+  /*
+  * Kalman Filter update and prediction math lives in here
   */
   KalmanFilter ekf_;
 
@@ -40,10 +40,6 @@ private:
 
   // tool object used to compute Jacobian and RMSE
   Tools tools;
-  Eigen::MatrixXd R_laser_;
-  Eigen::MatrixXd R_radar_;
-  Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
 };
 
 #endif /* FusionEKF_H_ */
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,40 +3,68 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
-KalmanFilter::KalmanFilter() {}
+KalmanFilter::KalmanFilter() {
+
+    // lidar observation noise fixed
+    R_lidar_ = MatrixXd(2, 2);
+    R_lidar_ << 0.0225, 0.0000,
+                0.0000, 0.0225 ;
+
+    // Lidar observation model
+    H_lidar_ = MatrixXd( 2 , 4 ) ;
+    H_lidar_ << 1, 0, 0, 0,
+                0, 1, 0, 0 ;
+
+
+    // radar observation model
+    // to be computed at each radar update
+
+    H_radar_  = MatrixXd( 3 , 4 ) ;
+    H_radar_ << 0 , 0 , 0 , 0 ,
+                0 , 0 , 0 , 0 ,
+                0 , 0 , 0 , 0 ;
+
+    // radar observation noise fixed
+    R_radar_ = MatrixXd(3, 3);
+    R_radar_ << 0.09, 0.0000, 0.00,
+                0.00, 0.0009, 0.00,
+                0.00, 0.0000, 0.09 ;
+
+    // P = error covariance matrix
+    P_ = MatrixXd(4, 4);
+    P_ << 1, 0, 0    , 0 ,
+          0, 1, 0    , 0 ,
+          0, 0, 225  , 0 ,
+          0, 0, 0    , 225 ;
+
+    // State transition matrix - without [dt]
+    F_ = MatrixXd(4, 4);
+    F_ << 1, 0, 0 , 0 ,
+          0, 1, 0 , 0 ,
+          0, 0, 1 , 0 ,
+          0, 0, 0 , 1 ;
+
+    // process noise
+    Q_ = MatrixXd(4, 4);
+
+    // a 4x4 identity matrix
+    I_ = MatrixXd::Identity( 4 , 4  ) ;
+
+
+    prev_dt = -1.0  ;
+
+
+
+}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+void KalmanFilter::Predict( double dt ) {
+
+
+
 }
 
-void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-}
-
-void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
-}
-
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
-}
